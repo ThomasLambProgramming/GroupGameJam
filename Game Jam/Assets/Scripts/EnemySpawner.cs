6490db1c5b50730082ba093ba5a1b4ad7d745291
Thomas Lamb <mcsorleyprogramming@gmail.com>
2021-06-24 10:39:04 +1000

    Basic spawner made

--- Game Jam/Assets/Scripts/EnemySpawner.cs
+++ Game Jam/Assets/Scripts/EnemySpawner.cs
@@ -8,9 +8,20 @@ public class EnemySpawner : MonoBehaviour
     
     public float enemyMoveSpeed = 10f;
     public float enemySpawnSpeed = 2f;
-    
-    public Vector3 minSpawnLocation = new Vector3(0, 0, 0);
-    public Vector3 maxSpawnLocation = new Vector3(0, 0, 0);
+    public float ySpawnHeight = 0.15f;
+
+    //small box of directions to give the enemies better movement
+    public Vector3 minMoveLocation = new Vector3(0, 0, 0);
+    public Vector3 maxMoveLocation = new Vector3(0, 0, 0);
+
+    public float xSpawnPosition = 0;
+    public float zSpawnPosition = 0;
+
+    //spawn ranges of the walls
+    public float minXSpawn = 0;
+    public float maxXSpawn = 0;
+    public float minZSpawn = 0;
+    public float maxZSpawn = 0;
 
     private float spawnTimer = 0;
 
@@ -23,39 +34,49 @@ public class EnemySpawner : MonoBehaviour
             spawnTimer = 0;
 
             //what sides to spawn the enemy to
-            int xPos = Random.Range(0, 2);
-            int zPos = Random.Range(0, 2);
-
+            int wallChoice = Random.Range(0, 4);
+            
             Vector3 spawnLocation = Vector3.zero;
-            //negative side of wall
-            if (xPos == 0)
+            //Left wall
+            if (wallChoice == 0)
             {
-
+                spawnLocation = new Vector3(xSpawnPosition, ySpawnHeight, Random.Range(minZSpawn, maxZSpawn));
             }
-            //positive side of wall
-            else
+            //Right wall
+            else if (wallChoice == 1)
             {
-
+                spawnLocation = new Vector3(-xSpawnPosition, ySpawnHeight, Random.Range(minZSpawn, maxZSpawn));
             }
-            //negative side of wall
-            if (zPos == 0)
+            //Up wall
+            else if (wallChoice == 2)
             {
-
+                spawnLocation = new Vector3(Random.Range(minXSpawn, maxXSpawn), ySpawnHeight, zSpawnPosition);
             }
-            //positive side of wall
-            else
+            //Down wall
+            else if (wallChoice == 3)
             {
-
+                spawnLocation = new Vector3(Random.Range(minXSpawn, maxXSpawn), ySpawnHeight, -zSpawnPosition);
             }
 
 
             //Spawn enemy
-            GameObject holder = Instantiate(enemyPrefab, Vector3.zero, 
+            GameObject holder = Instantiate(enemyPrefab, spawnLocation, 
                 Quaternion.identity, transform);
 
             Enemy tempEnemy = holder.GetComponent<Enemy>();
-            tempEnemy.moveDirection = new Vector3(Random.Range(-1.0f, 1.0f), 0, Random.Range(-1.0f, 1.0f));
-            tempEnemy.moveDirection = tempEnemy.moveDirection.normalized;
+            
+            //have a area in the middle to make all enemys move towards that (so they dont spawn and go straight for a wall behind them)
+            Vector3 direction = new Vector3(
+                Random.Range(minMoveLocation.x, maxMoveLocation.x), 
+                0, 
+                Random.Range(minMoveLocation.z, maxMoveLocation.x));
+            Vector3 enemyPosition = tempEnemy.transform.position;
+            
+            //set to 0 so its only moving along the x and z axis as it is top down
+            enemyPosition.y = 0;
+
+            tempEnemy.moveDirection = (direction - enemyPosition).normalized;
+            
         }
     }
 }
