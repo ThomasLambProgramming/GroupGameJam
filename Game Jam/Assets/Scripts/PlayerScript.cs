c53ed3676b0b489ecda43c9662d89e7ea3f8750a
Thomas Lamb <mcsorleyprogramming@gmail.com>
2021-06-24 10:56:40 +1000

    Updated Scene

--- Game Jam/Assets/Scripts/PlayerScript.cs
+++ Game Jam/Assets/Scripts/PlayerScript.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerScript : MonoBehaviour
 {
     // Start is called before the first frame update
+    public int playerHealth = 3;
     void Start()
     {
         
@@ -13,6 +14,17 @@ public class PlayerScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (playerHealth <= 0)
+        {
+            //Game over
+            transform.position = new Vector3(0, 6, 0);
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform.CompareTag("Enemy"))
+        {
+            playerHealth--;
+        }
     }
 }
